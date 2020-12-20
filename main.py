f7ff940a416fb375b8552f97843838bcc29d4f10
TUSHER38 <76174561+TUSHER38@users.noreply.github.com>
2020-12-21 00:12:12 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+print("\033[93")
+import os,time,sys,shutil
+
+class Main:
+
+	def __init__(self):
+		self.detekos()
+
+	def menu(self):
+		print("""
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		;       S M S  B O M B      ;
+		;---------------------------;
+		;       Author : toufiqul     ;
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+NOTE: These tools are for Tarmax users only, no one will use them for bad.
+
+1. india
+2. indonesia
+3. bangladash
+4. united states
+5. pakistan
+""")
+		pilih=int(input('Enter target namber/> '))
+		if pilih == 1:
+			import src.payu
+		elif pilih == 2:
+			import src.matahari
+		elif pilih == 3:
+			import src.alodok
+		elif pilih == 4:
+			import src.olx
+		elif pilih == 5:
+			import src.socil
+		else: print("[!] lihat menu dong(o)");self.menu()
+
+	def detekos(self):
+		#remove cache
+		try:
+			shutil.rmtree("src/__pycache__")
+		except: pass
+
+		if os.name in ['nt','win32']:
+			os.system('cls')
+		else: os.system('clear')
+		self.menu()
+
+try:
+	Main()
+except KeyboardInterrupt:
+	exit('[Exit] Key interrupt')
+except Exception as F:
+	print('Err: %s'%(F))
